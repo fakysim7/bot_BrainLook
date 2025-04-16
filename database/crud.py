8cb66cf0f6f03bc603696000b5b1598e225e0124
fakysim7 <81712937+fakysim7@users.noreply.github.com>
2025-04-16 15:22:01 +0300

    Update crud.py

--- database/crud.py
+++ database/crud.py
@@ -1,18 +1,19 @@
 from sqlalchemy.orm import Session
 from database.models import Event
+from database.connection import get_sync_session  # импортируем функцию создания сессии
 
-async def create_event(title, date, time, place, address, event_type, guests):
-    db = Session()
-    event = Event(
-        title=title,
-        date=date,
-        time=time,
-        place=place,
-        address=address,
-        event_type=event_type,
-        guests=guests
-    )
-    db.add(event)
-    db.commit()
-    db.refresh(event)
-    db.close()
+def create_event(title, date, time, place, address, event_type, guests):
+    with get_sync_session() as db:  # Контекстный менеджер, автоматически закроет сессию
+        event = Event(
+            title=title,
+            date=date,
+            time=time,
+            place=place,
+            address=address,
+            event_type=event_type,
+            guests=guests
+        )
+        db.add(event)
+        db.commit()
+        db.refresh(event)
+        return event
