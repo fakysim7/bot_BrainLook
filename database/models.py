4d46531470aad784a7515e6e75292965ac31c01f
fakysim7 <81712937+fakysim7@users.noreply.github.com>
2025-04-16 18:07:47 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,13 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from database.connection import Base
+from sqlalchemy import Column, Integer, String, Date, Time, JSON
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
 
 class Event(Base):
-    __tablename__ = 'events'
+    __tablename__ = "events"
+
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    date = Column(String)
-    time = Column(String)
-    place = Column(String)
-    address = Column(String)
-    event_type = Column(String)
-    guests = Column(String)
+    title = Column(String, nullable=False)
+    date = Column(Date, nullable=False)
+    time = Column(Time, nullable=False)
+    place = Column(String, nullable=False)
+    address = Column(String, nullable=True)
+    event_type = Column(String, nullable=False)
+    guests = Column(JSON, nullable=True)  # список гостей
